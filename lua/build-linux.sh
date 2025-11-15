d179ca006176467eb8c9b26db891107532b32d4e
DarkCaster <dark.caster@outlook.com>
2025-11-15 20:34:30 +0300

    lua: build scripts updated

--- lua/build-linux.sh
+++ lua/build-linux.sh
@@ -7,14 +7,10 @@ cd "$script_dir/build/lua-"*
 
 patch -p1 -i ../../build.patch
 
-#  CC="x86_64-linux-gnu-gcc -std=gnu99" \
-#  AR="x86_64-linux-gnu-ar rcu" \
-#  RANLIB="x86_64-linux-gnu-ranlib" \
-
 make \
   PLAT=linux \
   MYCFLAGS="-fPIC" \
   MYLDFLAGS="-static -fPIC"
 
-x86_64-linux-gnu-strip --strip-unneeded src/lua
+strip --strip-unneeded src/lua
 cp src/lua ../../lua-linux-$arch
