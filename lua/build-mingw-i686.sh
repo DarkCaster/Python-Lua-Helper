031263dd3591381865a1de688d7cd14e42a858c5
DarkCaster <dark.caster@outlook.com>
2025-11-15 17:23:20 +0300

    lua: add mingw i686 build script

---
+++ lua/build-mingw-i686.sh
@@ -0,0 +1,18 @@
+set -e
+
+script_dir="$(cd "$(dirname "$0")" && pwd)"
+
+pushd "$script_dir/build/lua-"*
+
+patch -p1 -i ../../build.patch
+
+make \
+  PLAT=mingw \
+  CC="i686-w64-mingw32-gcc -std=gnu99" \
+  AR="i686-w64-mingw32-ar rc" \
+  RANLIB="i686-w64-mingw32-ranlib"
+
+i686-w64-mingw32-strip --strip-unneeded src/lua.exe
+cp src/lua.exe ../../lua-windows-i686.exe
+
+popd
