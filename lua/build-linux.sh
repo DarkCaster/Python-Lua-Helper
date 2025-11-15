a799fd62027f41c86520e7ae6abf3f7ecb34dcb4
DarkCaster <dark.caster@outlook.com>
2025-11-15 14:49:55 +0300

    lua: add linux build script

---
+++ lua/build-linux.sh
@@ -0,0 +1,22 @@
+set -e
+
+script_dir="$(cd "$(dirname "$0")" && pwd)"
+arch=$(arch)
+
+pushd "$script_dir/build/lua-"*
+
+patch -p1 -i ../../no-dlopen.patch
+
+#  CC="x86_64-linux-gnu-gcc -std=gnu99" \
+#  AR="x86_64-linux-gnu-ar rcu" \
+#  RANLIB="x86_64-linux-gnu-ranlib" \
+
+make \
+  PLAT=linux \
+  MYCFLAGS="-fPIC" \
+  MYLDFLAGS="-static -fPIC"
+
+x86_64-linux-gnu-strip --strip-unneeded src/lua
+cp src/lua ../../lua-linux-$arch
+
+popd
