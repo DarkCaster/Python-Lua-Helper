e998392583b3062dc8757be12220f887afbef030
DarkCaster <dark.caster@outlook.com>
2025-11-15 11:21:50 +0300

    lua: download-lua.sh updated

--- lua/download-lua.sh
+++ lua/download-lua.sh
@@ -11,3 +11,9 @@ cd "$script_dir"
 
 [[ ! -e "$lua_src" ]] && echo "downloading $lua_src" && curl -s -L -o "$script_dir/$lua_src" "https://www.lua.org/ftp/$lua_src"
 echo "checking $lua_src" && sha256sum -c "$lua_checksum"
+
+rm -rf "$script_dir/build"
+mkdir -p "$script_dir/build"
+
+cd "$script_dir/build"
+tar xf "$script_dir/$lua_src"
