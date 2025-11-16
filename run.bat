d688b85dd2013ec43a9b0ac5016d5fca28268dd6
DarkCaster <dark.caster@outlook.com>
2025-11-16 20:54:23 +0300

    Added run.bat helper script

---
+++ run.bat
@@ -0,0 +1,14 @@
+@echo off
+setlocal
+
+set "script_dir=%~dp0"
+set "venv_dir=%script_dir%venv"
+
+if not exist "%venv_dir%" (
+    python -m venv "%venv_dir%"
+    "%venv_dir%\Scripts\python" -m pip --require-virtualenv install --upgrade pip build
+)
+
+"%venv_dir%\Scripts\python" %*
+
+endlocal
