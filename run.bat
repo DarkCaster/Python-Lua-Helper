d066e63be54bc6381bde06fa8c1e3b90e5fa67c8
DarkCaster <dark.caster@outlook.com>
2025-11-17 15:18:43 +0300

    run.sh and run.bat updated

--- run.bat
+++ run.bat
@@ -5,10 +5,10 @@ set "script_dir=%~dp0"
 set "venv_dir=%script_dir%venv"
 
 if not exist "%venv_dir%" (
-    python -m venv "%venv_dir%"
-    "%venv_dir%\Scripts\python" -m pip --require-virtualenv install --upgrade pip build
+    python3 -m venv "%venv_dir%"
+    "%venv_dir%\Scripts\python3" -m pip --require-virtualenv install --upgrade pip build
 )
 
-"%venv_dir%\Scripts\python" %*
+"%venv_dir%\Scripts\python3" %*
 
 endlocal
--- run.sh
+++ run.sh
@@ -5,8 +5,8 @@ script_dir="$(cd "$(dirname "$0")" && pwd)"
 venv_dir="$script_dir/venv"
 
 if [[ ! -d "$venv_dir" ]]; then
-  python -m venv "$venv_dir"
-  "$venv_dir/bin/python" -m pip --require-virtualenv install --upgrade pip build
+  python3 -m venv "$venv_dir"
+  "$venv_dir/bin/python3" -m pip --require-virtualenv install --upgrade pip build
 fi
 
-"$venv_dir/bin/python" "$@"
+"$venv_dir/bin/python3" "$@"
